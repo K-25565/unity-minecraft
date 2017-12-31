b4dd76de8afea6e1ae4bd04c430373f97445f3f1
K_25565 <34975675+K-25565@users.noreply.github.com>
2017-12-30 22:05:44 -0700

    Add files to Github from UnityProject folder
    Part 1

---
+++ Assets/Scripts/Camera/CameraFollowMouse.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowMouse : MonoBehaviour
+{
+    // Public Variables
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationY = 0F;
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        gameObject.GetComponent<Rigidbody>().freezeRotation = true;
+        
+        // Check if a ridgedbody exists
+        if (gameObject.GetComponent<Rigidbody>() == true)
+        {
+            // Freeze the ridgedbody in place.
+            gameObject.GetComponent<Rigidbody>().freezeRotation = true;
+        }
+        else
+        {
+            // Print an error message.
+            print("Error!  There is no ridgedbody attached to the player!!!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+    }
+
+}
---
+++ Assets/Scripts/World Generation/GenBasicWorld.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenBasicWorld : MonoBehaviour
+{
+    // Public Variables
+    public int ChunkSizeX = 0;
+    public int ChunkSizeY = 0;
+    public int ChunkSizeZ = 0;
+
+    // World Gen Options
+    public int DirtLayerSize = 0;
+
+    // Block Inventory
+    public Object GrassMat = null;
+    public Object DirtMat = null;
+    public Object StoneMat = null;
+    public Object BedrockMat = null;
+
+	// Use this for initialization
+	void Start ()
+    {       
+        GenerateChunk();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void GenerateChunk()
+    {
+        for (int x = 0; x < ChunkSizeX; x++)
+        {
+            for (int y = 0; y < ChunkSizeY; y++)
+            {
+                for (int z = 0; z < ChunkSizeZ; z++)
+                {
+                    if (y == (ChunkSizeY - 1))
+                    {
+                        Instantiate(GrassMat, new Vector3(x, y, z), Quaternion.identity);
+                    }
+                    else if (y > (ChunkSizeY - DirtLayerSize))
+                    {
+                        Instantiate(DirtMat, new Vector3(x, y, z), Quaternion.identity);
+                    }
+                    else if (y > 0)
+                    {
+                        Instantiate(StoneMat, new Vector3(x, y, z), Quaternion.identity);
+                    }
+                    else
+                    {
+                        Instantiate(BedrockMat, new Vector3(x, y, z), Quaternion.identity);
+                    }
+                }
+            }
+        }
+    }
+}
