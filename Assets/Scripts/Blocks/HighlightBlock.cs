c956a8ecff5d4b6d3e34495088f87dedc1486454
K_25565 <34975675+K-25565@users.noreply.github.com>
2017-12-31 22:12:56 -0700

    Highlighted Blocks Part 1

---
+++ Assets/Scripts/Blocks/HighlightBlock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightBlock : MonoBehaviour
+{
+    private void OnMouseOver()
+    {
+        gameObject.GetComponentInChildren<Renderer>().material.shader = Shader.Find("FX/Flare");    
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponentInChildren<Renderer>().material.shader = Shader.Find("Standard");
+    }
+}
